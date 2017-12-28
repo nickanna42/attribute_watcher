f63c80556f17374571c720e6f9245f86404c605e
Nicholas Anna <nickanna_42@yahoo.com>
2017-12-28 14:11:37 -0500

    Update lin_alg.js

--- lin_alg.js
+++ lin_alg.js
@@ -1,9 +1,4 @@
-'use strict';
-var linAlg = {
-    arrayDims : function(x) {
-        var output = 0;
-        if (!Array.isArray(x)) {
-            // Nicholas Anna.
+// Nicholas Anna.
 // Dec 2017
 // A package that emulates much of the function of NumPy with plain JS arrays.
 
@@ -208,87 +203,5 @@ var linAlg = {
             output = input;
         }
         return output;
-    }
-};
- undefined;
-        }
-        if (x.length == 0) {
-            output++;
-            return output;
-        } else {
-            output++;
-        }
-        if (Array.isArray(x[0])) {
-            output = output + this.arrayDims(x[0]);
-            return output;
-        } else {
-            return output;
-        }
-    },
-
-    arraySize : function(x) {
-        var output = [];
-        if (!Array.isArray(x)) {
-            return undefined;
-        }
-        output.push(x.length);
-        if (x.length >= 1) {
-            if (Array.isArray(x[0])) {
-                output = output.concat(this.arraySize(x[0]));
-            }
-        }
-        return output;
-    },
-
-    multiply : function(x, y) {
-        var output = [];
-        if (typeof x == 'number' && typeof y == 'number') {
-            return x * y;
-        }
-        if (typeof x == 'number' && Array.isArray(y)) {
-            if (y.length == 0) {
-                return output;
-            }
-            for (var i = 0; i < y.length; i++) {
-                if (typeof y[i] == 'number') {
-                    output.push(x * y[i]);
-                } else if (Array.isArray(y[i])) {
-                    output.push(this.multiply(x, y[i]));
-                }
-            }
-            return output;
-        }
-        if (Array.isArray(x) && typeof y == 'number') {
-            if (x.length == 0) {
-                return output;
-            }
-            for (var i = 0; i < x.length; i++) {
-                if (typeof x[i] == 'number') {
-                    output.push(y * x[i]);
-                } else if (Array.isArray(x[i])) {
-                    output.push(this.multiply(y, x[i]));
-                }
-            }
-            return output;
-        }
-    },
-
-    zeros : function(x, zero) {
-        var output = [];
-        var y = [];
-        if (x.length > 1) {
-            for (var i = 1; i < x.length; i++) {
-                y.push(x[i]);
-            }
-            for (var i = 0; i < x[0]; i++) {
-                output.push(this.zeros(y, zero));
-            }
-            return output;
-        } else {
-            for (var i = 0; i < x[0]; i++) {
-                output.push(zero);
-            }
-            return output;
-        }
     }
 };
